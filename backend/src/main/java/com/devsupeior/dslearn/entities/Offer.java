fe0b5fb03e55f9d5bfa30f5e3ff090301cd27574
nabucodonosor-java <nabucodonosor.java@gmail.com>
2021-07-14 09:22:07 -0300

    05-05 Course

---
+++ backend/src/main/java/com/devsupeior/dslearn/entities/Offer.java
@@ -0,0 +1,25 @@
+package com.devsupeior.dslearn.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_offer")
+public class Offer implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String edition;
+	private Instant startMoment;
+	private Instant endMoment;
+	
+
+}
